326474dea8009e7498f95469869122d2d2759d86
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-12 03:05:39 +0530

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 
  export const App = () => {
-  return <h1>Hello,Pallavi Srivatsva.
-   </h1>
+  return (
+    <div>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </div>
+  );
+};
+
+const NetflixSeries = () => {
+   return (
+      <div>
+      <div>
+         <img src="/images/qot.jpg" alt="jpg" width="40%" height="40%" />
+      </div>
+      <h2>Name: Queen of Tears</h2>
+      <h3>Rating:8.2</h3>
+      <p>
+         Summary: Baek Hyun-woo and Hong Hae-in naviagte a tense relationship,
+         both at home and at work. But upon deciding his future, Hyun-woo pays a 
+         visit to his family.
+      </p>
+    </div>
+   );
 };
