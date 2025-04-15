fb4155fbddfd59eacd8fa45264b598a3f3a21f90
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-15 09:27:40 +0530

    new code

---
+++ src/components/NetflixSeries.jsx
@@ -0,0 +1,30 @@
+ import seriesData from "../api/seriesData.json";
+ const NetflixSeries = () => {
+    return (
+       <ul>
+        {
+            seriesData.map((curElem) => {
+              return(
+              <li key={curElem.id}>
+                <div>
+                   <img src={curElem.img_url} alt="jpg" width="40%" height="40%" />
+                </div>
+                <h2>Name: {curElem.name}</h2>
+                <h3>Rating:{curElem.rating}</h3>
+                <p>
+                   Summary: {curElem.description}
+                </p>
+                <p>Genre:{curElem.genre}</p>
+                <p>Cast: {curElem.cast}</p>
+                <a href={curElem.watch_url} target="_blank">
+                <button>Watch Now</button>
+                </a>
+                </li>
+              );
+            })}
+        
+     </ul>
+    );
+ };
+
+ export default NetflixSeries;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import { App } from './App.jsx';
-import { Practices } from "./Practices.jsx";
+import { App } from './App.jsx';
+// import { Practices } from "./Practices.jsx";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Practices />
+    <App />
+    {/* <Practices /> */}
   </React.StrictMode>
 );
