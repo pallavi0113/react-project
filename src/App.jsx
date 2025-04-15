78227240ce607824dd88fda014edddcbb2803013
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-15 10:05:04 +0530

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 // import {Fragment} from "react";
  import NetflixSeries from "./components/NetflixSeries";
 export const App = () => {
-  return (
-    <>
-      <NetflixSeries />
-      {/* <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries /> */}
-    </>
-   );
+  return <NetflixSeries />;
 };
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,20 @@
+export const SeriesCard = ({curElem}) => {
+    const {img_url,name,rating,description,cast,genre,watch_url} = curElem;
+    return(
+        <li>
+          <div>
+             <img src={img_url} alt={name} width="40%" height="40%" />
+          </div>
+          <h2>Name: {name}</h2>
+          <h3>Rating:{rating}</h3>
+          <p>
+             Summary: {description}
+          </p>
+          <p>Genre:{genre}</p>
+          <p>Cast: {cast}</p>
+          <a href={watch_url} target="_blank">
+          <button>Watch Now</button>
+          </a>
+        </li>
+    );
+};
